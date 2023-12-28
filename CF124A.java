a5fc67a001ef107c062ce751a20a1ebba23030b7
Pranshu Raj <rajpranshu2222@gmail.com>
2023-12-28 11:19:48 +0530

    done

---
+++ CF124A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CF124A {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(Math.min(n-a,b+1));
+        input.close();
+    }
+
+}
